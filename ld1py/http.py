c5c1060679dcdb9645a2d889eee12153fe2fc020
Ismail Coskuner <coskuner.ismail@gmail.com>
2021-12-26 16:50:23 -0800

    wrap requests methods with retry

--- ld1py/http.py
+++ ld1py/http.py
@@ -54,3 +54,43 @@ class WithRetries:
             raise ApiRequestError(resp, error, args, kwargs)
 
         return retry
+
+
+def get(*args, retries=0, sleep=10, **kwargs):
+    """Wrapper around requests.get method using retry decorator"""
+
+    @WithRetries(retries=retries, sleep=sleep)
+    def _get(*args, **kwargs):
+        return requests.get(*args, **kwargs)
+
+    return _get(*args, **kwargs)
+
+
+def post(*args, retries=0, sleep=10, **kwargs):
+    """Wrapper around requests.post method using retry decorator"""
+
+    @WithRetries(retries=retries, sleep=sleep)
+    def _post(*args, **kwargs):
+        return requests.post(*args, **kwargs)
+
+    return _post(*args, **kwargs)
+
+
+def put(*args, retries=0, sleep=10, **kwargs):
+    """Wrapper around requests.put method using retry decorator"""
+
+    @WithRetries(retries=retries, sleep=sleep)
+    def _put(*args, **kwargs):
+        return requests.put(*args, **kwargs)
+
+    return _put(*args, **kwargs)
+
+
+def delete(*args, retries=0, sleep=10, **kwargs):
+    """Wrapper around requests.delete method using retry decorator"""
+
+    @WithRetries(retries=retries, sleep=sleep)
+    def _delete(*args, **kwargs):
+        return requests.delete(*args, **kwargs)
+
+    return _delete(*args, **kwargs)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ld1py",
-    version="0.0.7",
+    version="0.0.8",
     author="LD1 Labs",
     author_email="info@ld1labs.com",
     description="Common python utilities for LD1 Labs projects",
